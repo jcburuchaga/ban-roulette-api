de0aab65d60545ecc9826b505e216653673aee94
jcburuchagab <juan.buruchaga@multicaja.cl>
2019-08-19 18:26:40 -0400

    add some security validations to roll

--- src/api/index.js
+++ src/api/index.js
@@ -8,9 +8,10 @@ const userService = require('../service/userService');
 const chips_permited = [1,10,100];
 router.get('/balance',middlewares.checkToken, (req, res) => {
 
-  let address = await bananojs.getAccountBalance("get account")
+  let balance = await bananojs.getAccountBalance("get account")
+  balance = await bananojs.rawToBan(balance);
   res.json({
-    balance: address.balance 
+    balance: balance
   });
 });
 
@@ -27,6 +28,7 @@ let bet = await betService.createBet('user_id',client_hash,server_hash,roll_numb
 
 router.post('/roll',middlewares.checkToken, async (req, res) => {
   let hash = req.body.current_hash;
+  let account_address = "get account";
   let bet = betService.getBetByClientHash(hash);
   let number = parseInt(bet.result);
   if (!req.body.user_bet) {
@@ -41,39 +43,78 @@ router.post('/roll',middlewares.checkToken, async (req, res) => {
     });
     return;
   }
+  if (isNaN(chip_amount)) {
+    res.json({
+      error: 'error: chip_amount isNaN'
+    });
+  }
   if (!chips_permited.includes(chip_amount)) {
     res.json({
-      error: 'error: amount chip not permited'
+      error: 'error: amount per chip not permited'
     });
   }
   let user_bet = req.body.user_bet;
   let chip_amount = req.body.chip_amount;
   //check bet integrity
   let integrity_fail = false;
+
+
+  let message = "";
+  let bet_amount = 0;
   user_bet.forEach(b => {
     if (b.number == -1 || b.number == -2) {
       if (b.count < 3 || b.count > 9) {
         integrity_fail = true;
+        break;
       }
     }else{
       if (b.count < 1 || b.count > 5) {
         integrity_fail = true;
+        next();
+        break;
       }
     }
+    bet_amount = bet_amount + (b.count * parseInt(chip_amount));
+  }); 
+
+  if (integrity_fail) {
+    res.json({
+      error: 'bet integrity fail, rules are violated'
+    });
+    return;    
+  }
+  //check balance
+  let current_wallet_balance = await bananojs.getAccountBalance(account_address);
+  current_wallet_balance = bananojs.rawToBan(current_wallet_balance);
+  if (bet_amount > current_wallet_balance) {
+    res.json({
+      error: 'No balance in wallet'
+    });
+    return;    
+  }
+
+  //now all is clear get winings
+
+  let red_numbers = [32];
+  let win_amount = 0;
+
+  user_bet.forEach(b => {    
+    if (b.number == -1 || b.number == -2) {
+      
+    }else{
+      
+    }    
+  }); 
+
+
+  res.json({
+    win_amount: '0',
+    server_hash: '0',
+    message: message
   });
-  if (!integrity_fail) {
-    res.json({
-      win_amoun: '0',
-      server_hash: '0'
-    });
-    return;
-  }
-  else{
-    res.json({
-      error: 'bet integrity fail'
-    });
-    return;
-  }
+  return;
+  
+  
   
 });  
 
